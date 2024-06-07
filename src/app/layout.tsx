e334fcc6086bf100c4c34402ddf7af58f35f718e
FacuRamosGarde <facundoramosgarde@gmail.com>
2024-06-07 15:02:48 -0300

    add description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,8 +7,8 @@ import { HeaderSplit } from "../components/common/HeaderSplit"
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Split Bill",
-  description: "Split Bill",
+  title: "Split Bill - Divide tus cuentas de manera fácil y rápida",
+  description: "La división de cuentas nunca fue tan facil, Split Bill te ayuda a dividir tus cuentas de manera rapida y sencilla",
 };
 
 export default function RootLayout({
